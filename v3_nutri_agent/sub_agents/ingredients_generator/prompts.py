0489b8e40fdce354c46d682c0a60b698214a894d
Aurobindo Tripathy <aurobindotripathy@mac.lan>
2026-01-27 05:57:43 -0800

    simpler prompt

--- v3_nutri_agent/sub_agents/ingredients_generator/prompts.py
+++ v3_nutri_agent/sub_agents/ingredients_generator/prompts.py
@@ -12,26 +12,21 @@ ts_from_off_grouped' tool. Do not answer from your internal knowledge.
 d its ingredients"
 
 **Required Sequence and Workflow:**
-   **Step 1:** The input is expected to contain a food item name and optionally, a disease name potentially impacted by the food item.
+    **Step 1:** The input is expected to contain a food item name and optionally, a disease name potentially impacted by the food item.
       You are first tasked to extract the food item from the input and, optionally, the disease/ailment name, if present.
       If the food item is a string or a phrase, then use it as-is.
       If the food item is the picture of a nutrition label, then OCR it and extract the ingredients.
       If the food item is bar code label, then extract the code.
       If the food item is a picture, then identify the food item.
 
-   **Step 2:**. Next, you are tasked to search for the nutrients that constitute the food item.
+    **Step 2:**. Next, you are tasked to search for the nutrients that constitute the food item.
       You MUST use tools in the following priority order;
       - First, you MUST call the tool 'get_nutriments_from_open_food_facts_grouped'. This takes the food item as input and returns a dictionary of nutrients.
       - Then, if the API call does not yield any result and returns an empty dictionary, only then you must use the 'google_search_tool' built-in tool to find the ingredients. 
-        When you use the google_search_tool, be sure to request the ingredient's ratio or proportions in the search query abd maintain the same format as the output of the 'get_nutriments_from_open_food_facts_grouped' tool. 
-      You MUST use one of the two tools, not both.
+        When you use the google_search_tool, be sure to request the ingredient's proportions in the search query and mainitain a JSON format as the output.
       - You MUST use the tools in the order specified, not in any other order.
-
-**Output**
-     The output is a dictionary of nutrients and the disease name if available in the input.
-     """
+"""
 
 INGREDIENTS_GENERATOR_DESCRIPTION = """
 Handles the nutrients in food items. Optionally, it can also be provided with a disease or ailment by the user, If so, it is to extract the ailment and pass on to another agent.
 """
-
